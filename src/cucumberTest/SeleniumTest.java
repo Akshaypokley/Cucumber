effc7f244b103efe92e15acf39226d5c3bdb8afb
AKSHAY POKLEY <akshay85pokley@gmail.com>
2018-05-28 22:47:36 +0530

    jhg

---
+++ src/cucumberTest/SeleniumTest.java
@@ -0,0 +1,33 @@
+package cucumberTest;
+/*
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by AKSHAY on 28/05/2018.
+ */
+/*public class SeleniumTest {
+   static WebDriver driver;
+    public static void  main(String args[])
+    {
+
+        System.setProperty("webdriver.gecko.driver","Driver/geckodriver.exe");
+
+        driver=new FirefoxDriver();
+
+        driver.manage().window().maximize();
+        driver.get("http://218.248.45.162:8081/BPAMSClient/login.aspx");
+
+        driver.findElement(By.id("txtLN")).sendKeys("de1234");
+
+        driver.findElement(By.id("txtPW")).sendKeys("Admin@123");
+
+        driver.findElement(By.id("btnLog")).click();
+
+
+
+    }
+
+}*/
